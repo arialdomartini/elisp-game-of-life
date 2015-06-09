bc425fe408a52084b64ca1a12b5c50febb0ea91c
Arialdo Martini <arialdomartini@gmail.com>
2015-06-09 13:35:37 +0200

    can predict if a cell should die

--- gameoflife.el
+++ gameoflife.el
@@ -29,3 +29,11 @@
 
 
 (count-alive-neighbors '(10 11) '( (10 10) (11 10) (12 10)))
+
+(defun should-die (cell generation)
+  (not (equal (count-alive-neighbors cell generation) 2))
+  )
+
+(eq (should-die '(5 5) '()) 't)                        ; it dies for loneliness
+(eq (should-die '(5 5) '( (4 4) (5 4))) nil)           ; it doesn't die
+(eq(should-die '(5 5) '( (4 4) (5 4) (6 4))) 't)       ; it dies for suffocation
